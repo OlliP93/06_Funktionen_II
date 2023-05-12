74a1d70275527c43340a9645ef13c1941c0fa510
OlliP93 <oliverpoepken@gmx.de>
2023-05-12 12:11:37 +0200

    Calc

--- script_01.js
+++ script_01.js
@@ -12,13 +12,111 @@
 */
 
 
+// // Konstanten
+const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
+const ERROR_STR_GEN = "Irgendetwas ging schief!"
+
+startApp()
+function startApp() {
+	output(calculator(getNum(),getNum(),getOp()));
+
+}
+
+function getNum() {
+	return parseInt(prompt("Zahl?"));
+
+}
+
+function getOp() {
+	return prompt("Operator?");
+
+}
+
+// agreement : "+","-","*",":","/"
+// output(calculator(3,2,"+"));
+// output(calculator(3,2,"-"));
+// output(calculator(3,2,"*"));
+// output(calculator(3,2,":"));
+// output(calculator(3,2,"/"));
+// output(calculator(3,0,"/"));
+// output(calculator(3,2,"#?!"));
+
+function calculator(a,b,op) {
 
 
+	switch (op) {
 
-// module: output | test:
-// output("hello");
-// output(2);
+
+		case "+":
+			return add(a,b);
+
+		case "-": 
+			return subtract(a,b);
+	
+		case "*": 
+			return multiply(a,b);
+
+		case "/":
+		case ":":
+			return divide(a,b);
+
+		default:
+			return ERROR_STR_GEN;
+	}
+
+	
+
+}
+
+// module: multiplication a * b |  test:
+
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+
+function multiply(a,b) {
+	return a * b;
+}
+// // module: subtraction a - b |  test:
+
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+
+function subtract(a,b) {
+	return a - b;
+}
+
+// module: division a / b |  test:
+
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(3,0));
+
+function divide(a,b) {
+	if (b != 0) {
+		return a/b;
+	} else {
+		return ERROR_STR_DIV;
+	}
+	
+}
+
+// // // module: addition a + b |  test:
+
+// // output(add(2,2));
+// // output(add(2,-2));
+// // output(add(2,0));
+
+function add(a,b) {
+	return a + b;
+}
+
+
+// // module: output | test:
 function output(outputData) {
+
 	console.log(outputData);
 }
   
